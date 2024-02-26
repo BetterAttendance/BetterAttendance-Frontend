2c119f53d455a5fcca064eab977ca7c906e69584
Andrew Jung <me@andrewjunggg.com>
2024-02-25 19:30:40 -0800

    Implement join page with regex validation. TODO: validation with backend

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,8 +1,35 @@
-import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react';
-import Link from 'next/link';
+'use client';
+
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Link,
+} from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+import { useMemo, useState } from 'react';
 import { IoCopy } from 'react-icons/io5';
 
 export default function Join() {
+  const router = useRouter();
+  const [sessionCode, setSessionCode] = useState<string>('');
+
+  const validateSessionCode = (sessionCode: string) =>
+    sessionCode.match(/^[A-Za-z0-9]{5}$/i);
+
+  const isInvalid = useMemo(() => {
+    if (sessionCode === '') return true;
+
+    return validateSessionCode(sessionCode) ? false : true;
+  }, [sessionCode]);
+
+  const handleJoinButton = () => {
+    // TODO: Validate session code from backend
+    router.push(`/session/${sessionCode}`);
+  };
+
   return (
     <>
       <Card className="p-5">
@@ -11,10 +38,25 @@ export default function Join() {
         </CardHeader>
         <CardBody className="flex items-center gap-3">
           Enter your session code
-          <Input></Input>
-          <Button color="primary" startContent={<IoCopy />}>
-            Join Session
-          </Button>
+          <Input
+            placeholder="#####"
+            value={sessionCode}
+            onValueChange={setSessionCode}
+            isInvalid={isInvalid}
+          />
+          {isInvalid ? (
+            <Button isDisabled color="primary" startContent={<IoCopy />}>
+              Join Session
+            </Button>
+          ) : (
+            <Button
+              color="primary"
+              startContent={<IoCopy />}
+              onClick={handleJoinButton}
+            >
+              Join Session
+            </Button>
+          )}
         </CardBody>
       </Card>
     </>
