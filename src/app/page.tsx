1a394feb0166f763e2275a1869eb9a0311e2d2b4
Andrew Jung <me@andrewjunggg.com>
2024-02-25 18:33:16 -0800

    Remove unnecessary imports, and add temporary styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import StartSessionButton from '@/components/StartSessionButton';
+import { Card, CardBody, CardHeader, Divider } from '@nextui-org/react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      Welcome to BetterAttendance!
-      <StartSessionButton />
-    </main>
+    <>
+      <Card className="p-5">
+        <CardHeader className="flex items-center">
+          <h4 className="text-large font-large">
+            Welcome to BetterAttendance!
+          </h4>
+        </CardHeader>
+        <CardBody className="flex items-center">
+          <StartSessionButton />
+          <Divider className="my-4" />
+          <Link href="join" className="text-blue-700">
+            I have a code
+          </Link>
+        </CardBody>
+      </Card>
+    </>
   );
 }
--- src/components/SessionLobby.tsx
+++ src/components/SessionLobby.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client';
 
-import React from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 import { useSocket } from '@/context/socket.context';
 
 export default function SessionLobby() {
-    const { socket, sessionID } = useSocket();
+  const { socket, sessionID } = useSocket();
 
-    return (
-        <div>
-            <main className="flex min-h-screen flex-col items-center p-24">
-                <h1>Welcome to session room: {sessionID}</h1>
-                <h2>To join the session please scan the QR code below</h2>
-          
-                <QRCodeCanvas value={`http://localhost:3000/session/${sessionID}`} />
+  return (
+    <>
+      <h1>Welcome to session room: {sessionID}</h1>
+      <h2>To join the session please scan the QR code below</h2>
 
-            </main>
-        </div>
-    );
+      <QRCodeCanvas value={`http://localhost:3000/session/${sessionID}`} />
+    </>
+  );
 }
