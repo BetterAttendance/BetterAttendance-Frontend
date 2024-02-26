0496ccf2e0f604e5caa5ef055a7dcfa6d32fe09b
Andrew Jung <me@andrewjunggg.com>
2024-02-25 18:51:42 -0800

    Add styling to SessionLobby component

--- src/components/SessionLobby.tsx
+++ src/components/SessionLobby.tsx
@@ -2,16 +2,23 @@
 
 import { QRCodeCanvas } from 'qrcode.react';
 import { useSocket } from '@/context/socket.context';
+import { Card, CardBody, CardHeader, Link } from '@nextui-org/react';
 
 export default function SessionLobby() {
   const { socket, sessionID } = useSocket();
 
   return (
     <>
-      <h1>Welcome to session room: {sessionID}</h1>
-      <h2>To join the session please scan the QR code below</h2>
-
-      <QRCodeCanvas value={`http://localhost:3000/session/${sessionID}`} />
+      <Card className="p-5">
+        <CardHeader>
+          <Link href="/">Back</Link>
+        </CardHeader>
+        <CardBody className="flex items-center">
+          <h1>Welcome to session room: {sessionID}</h1>
+          <h2>To join the session please scan the QR code below</h2>
+          <QRCodeCanvas value={`http://localhost:3000/session/${sessionID}`} />
+        </CardBody>
+      </Card>
     </>
   );
 }
