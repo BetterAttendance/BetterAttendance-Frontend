72708edf8f1c6bfe78a47f2db42aa0390dc30a61
khois345 <khois345@gmail.com>
2024-02-26 21:21:43 -0800

    First commit

--- src/components/SessionLobby.tsx
+++ src/components/SessionLobby.tsx
@@ -2,10 +2,14 @@
 
 import { QRCodeCanvas } from 'qrcode.react';
 import { useSocket } from '@/context/socket.context';
-import { Card, CardBody, CardHeader, Link } from '@nextui-org/react';
+import { Card, CardBody, CardHeader, Link, Button } from '@nextui-org/react';
 
 export default function SessionLobby() {
   const { socket, sessionID } = useSocket();
+
+  const handleStartQuiz = () => {
+    // ToDo: Emit event to start the quiz
+  }
 
   return (
     <>
@@ -15,8 +19,15 @@ export default function SessionLobby() {
         </CardHeader>
         <CardBody className="flex items-center">
           <h1>Welcome to session room: {sessionID}</h1>
-          <h2>To join the session please scan the QR code below</h2>
-          <QRCodeCanvas value={`http://localhost:3000/session/${sessionID}`} />
+          <h2 className='pb-5'>To join the session please scan the QR code below</h2>
+          <QRCodeCanvas 
+            value={`http://localhost:3000/session/${sessionID}`}
+            size={300} 
+            className='pb-5'/>
+            <Button type="submit" color="primary" variant="solid" 
+              onClick={handleStartQuiz}>
+                Start Quiz
+            </Button>
         </CardBody>
       </Card>
     </>
