a94b8eed9c03a427e813cdeb53356675bc5993f5
khois345 <khois345@gmail.com>
2024-03-11 17:11:55 -0700

    Fixing format

--- src/components/session_lobby/ClientSessionLobby.tsx
+++ src/components/session_lobby/ClientSessionLobby.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSocket } from '@/context/socket.context';
-import { Card, CardBody, CardHeader, Link } from '@nextui-org/react';
-import { Button } from '@nextui-org/react';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSocket } from "@/context/socket.context";
+import { Card, CardBody, CardHeader, Link } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 export default function ClientSessionLobby() {
-  const [username, setUsername] = useState<string>('');
+  const [username, setUsername] = useState<string>("");
   const { sessionCode } = useSocket();
   const [isQuizStarted, setIsQuizStarted] = useState<boolean>(false);
 
   useEffect(() => {
-    const username = localStorage.getItem('username');
+    const username = localStorage.getItem("username");
     if (username !== null) {
       setUsername(username as string);
     }
@@ -23,28 +23,21 @@ export default function ClientSessionLobby() {
           <Link href="/">Back</Link>
         </CardHeader>
         <CardBody className="flex items-center">
-            <>
-              <h1>
-                Hello {username}! Welcome to session room: {sessionCode}
-              </h1>
-              <h2 className="pb-5">
-                Please wait until the host start the quiz
-              </h2>
-              {isQuizStarted ? (
-                <Button type="submit" color="primary" variant="solid">
-                  Join Quiz
-                </Button>
-              ) : (
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="solid"
-                  isDisabled
-                >
-                  Join Quiz
-                </Button>
-              )}
-            </>
+          <>
+            <h1>
+              Hello {username}! Welcome to session room: {sessionCode}
+            </h1>
+            <h2 className="pb-5">Please wait until the host start the quiz</h2>
+            {isQuizStarted ? (
+              <Button type="submit" color="primary" variant="solid">
+                Join Quiz
+              </Button>
+            ) : (
+              <Button type="submit" color="primary" variant="solid" isDisabled>
+                Join Quiz
+              </Button>
+            )}
+          </>
         </CardBody>
       </Card>
     </>
--- src/context/user.context.tsx
+++ src/context/user.context.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useContext, createContext, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+import { useContext, createContext, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 interface UserContextType {
   generateNewUserId: () => void;
@@ -15,17 +15,17 @@ const UserProvider = (props: any) => {
   // TypeScript will complain if we don't declare null as a possible value for userID
 
   const generateNewUserId = () => {
-    localStorage.setItem('userId', nanoid());
-    console.log('New userId is generated.');
+    localStorage.setItem("userId", nanoid());
+    console.log("New userId is generated.");
   };
 
   useEffect(() => {
-    const userId = localStorage.getItem('userId');
+    const userId = localStorage.getItem("userId");
 
     if (!userId) {
       generateNewUserId();
     } else {
-      console.log('User already has a userId.');
+      console.log("User already has a userId.");
     }
   }, []); // Only call once in session page
 
