788227516becc0a14e988e02052f54c2b63f0d68
Andrew Jung <me@andrewjunggg.com>
2024-03-16 13:59:44 -0700

    Remove null state from the socket: fixes early closed connections.

--- src/context/socket.context.tsx
+++ src/context/socket.context.tsx
@@ -1,51 +1,40 @@
 'use client';
 
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 import EVENTS from '@/config/events';
 import SOCKET_CONFIG from '@/config/socket.config';
 
 interface Context {
-  socket: Socket | null;
+  socket: Socket;
   sessionCode: string;
   setSessionCode: (sessionCode: string) => void; // Exported function
 }
 
+const socket = io(SOCKET_CONFIG.SOCKET_URL, {
+  reconnection: true,
+  upgrade: true,
+  transports: ['websocket', 'polling'],
+});
+
 const SocketContext = createContext<Context>({
-  socket: null,
+  socket,
   sessionCode: '',
   setSessionCode: () => {},
 });
 
 const SocketsProvider = (props: any) => {
   const [sessionCode, setSessionCode] = useState<string>('');
-  const [socket, setSocket] = useState<Socket | null>(null);
 
-  useEffect(() => {
-    const clientSocket = io(SOCKET_CONFIG.SOCKET_URL, {
-      reconnection: true,
-      upgrade: true,
-      transports: ['websocket', 'polling'],
-    });
+  socket.on(EVENTS.SERVER.JOIN_SESSION, ({ sessionCode }) => {
+    console.log('Setting sessionCode: ', sessionCode);
+    setSessionCode(sessionCode);
+  });
 
-    setSocket(clientSocket);
-
-    return () => {
-      clientSocket.disconnect();
-    };
-  }, []);
-
-  if (socket) {
-    socket.on(EVENTS.SERVER.JOIN_SESSION, ({ sessionCode }) => {
-      console.log('Setting sessionCode: ', sessionCode);
-      setSessionCode(sessionCode);
-    });
-
-    socket.on(EVENTS.SERVER.LEAVE_SESSION, () => {
-      console.log('Setting sessionCode: ', sessionCode);
-      setSessionCode('');
-    });
-  }
+  socket.on(EVENTS.SERVER.LEAVE_SESSION, () => {
+    console.log('Setting sessionCode: ', sessionCode);
+    setSessionCode('');
+  });
 
   return (
     <SocketContext.Provider
